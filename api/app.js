e62779877f512f71b83c0500606bb7b2a6d6a8d4
EC2 Default User <ec2-user@ip-172-31-9-39.ec2.internal>
2023-05-10 04:07:24 +0000

    modify

--- api/app.js
+++ api/app.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/api/node1', (req, res) => {
-  res.status(200).send('Hello World!')
+  res.status(200).send('Hello World from node1')
 })
 
 app.get('/', (req, res) => {
-  res.status(500).send('eror')
+  res.status(500).send('eror 1')
 })
 
 app.listen(port, () => {
